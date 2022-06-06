4c68d2ec83bfe86b3a9ed0531134f6f98c4543ff
NaseemAhmadzai <66440183+NaseemAhmadzai@users.noreply.github.com>
2022-06-06 11:29:34 -0700

    Add files via upload

---
+++ frac.py
@@ -0,0 +1,315 @@
+###Fill in the body of the methods of the Fraction class below.
+###Some of them are filled in already for you so you don't have to do it.
+###
+###Name:
+###Date:
+
+class Fraction(object):
+    def __init__(self, str_fraction):
+        '''
+        Initializes a Fraction object
+
+        Parameter:
+        str_fraction is a string in the format of "num/denom". Assume it is properly formatted.
+        for example, if the string "-3/12" it will split the string by the slash, and then the
+        num(erator) is "-3" and the denom(inator) will be "12".
+
+        a Fraction object has two attributes:
+            self.num (integer, the "top part" of the string)
+            self.denom (integer, the "bottom part" of the string)
+            Assume the numerator and denominators are properly formatted and the denominator is not zero.
+        '''
+
+        frac = str_fraction.split("/")
+        self.num = eval(frac[0])
+        self.denom = eval(frac[1])
+
+        pass
+
+    def __str__(self):
+        '''
+        Returns a string format of the Fraction object in the form of "num/denom".
+        Does not reduce it - simply returns astring with its current num and denom values.
+        '''
+        return str(self.get_num()) + "/" + str(self.get_denom())
+
+    def __float__(self):
+        '''
+        Returns the floating point decimal equivalent of the fraction object.
+        For example:
+        my_frac = Fraction(1/4)
+        float(my_frac) will return 0.25
+        '''
+        ### The pass statement below causes the program to crash.
+        ### You need to return a floating point value.
+        return self.num / self.denom
+
+    def get_num(self):
+        '''
+        Returns the numerator of the fraction object.
+        '''
+        return self.num
+
+    def get_denom(self):
+        '''
+        Returns the denominator of the fraction object.
+        '''
+        return self.denom
+
+    def set_num(self, num):
+        '''
+        Sets self.num to num. Assume that num being passed in is an integer.
+        Returns nothing. Method finished for you already.
+        '''
+        self.num = num
+
+    def set_denom(self, denom):
+        '''
+        Sets self.denom to denom. Assume that num being passed in is an integer.
+        Returns nothing. Method finished for you already.
+        '''
+        self.denom = denom
+
+    def reduce(self):
+        '''
+        Reduces the attributes of the fraction to lowest terms, using the helper function find_gcf(a, b).
+        Returns a new fraction that is in reduced form.
+        '''
+        gcf = self.find_gcf(abs(self.num), abs(self.denom))
+        charge = 1
+        if self.num / self.denom <= 0:
+            charge = -1
+        return Fraction(f"{abs(self.num // gcf) * charge}/{abs(self.denom // gcf)}")
+
+    def find_gcf(self, a, b):
+        '''
+        Finds the greatest common factor between integers a and b.
+
+        Parameters:
+        a,b - are both integers.
+
+        Returns:
+        The integer GCF of a and b.
+        '''
+        gcf = 1
+        if a > b:
+            smaller = b
+        else:
+            smaller = a
+        for i in range(1, smaller + 1):
+            if ((a % i == 0) and (b % i == 0)):
+                gcf = i
+        return gcf
+
+    def get_reciprocal(self):
+        '''
+        Returns the reciprocal of the fraction as a new fraction object.
+        '''
+        return Fraction(f"{self.denom}/{self.num}")
+
+    #### Arithmetic operators ####
+    def __add__(self, f2):
+        '''
+        The implementation of adding two fractions together - "self + f2"
+        Parameters:
+        f2 - the other fraction you wish to add to "self".
+
+        Returns:
+        A new fraction where "self" and "f2" have been added together properly.
+        The result is in lowest terms.
+        '''
+
+        ###REMEMBER! To add fractions DO NOT add the numerators and denominators together.
+        ###To add or subtract fractions, they must have a common denominator.
+        ###HINT! You don't need to find the lowest common denominator.
+        ###Use any appropriate common denominator to do the calculation, then reduce it before returning it.
+
+        # todo:implement the actual addition of the Fraction data and return
+        # a new Fraction where we've added self and f2 together.
+
+        new_denom = f2.denom * self.denom
+        new_num = f2.denom * self.num + f2.num * self.denom
+
+        frac = Fraction(f"{new_num}/{new_denom}")
+
+        return frac.reduce()
+
+    def __sub__(self, f2):
+        '''
+        The implementation of subtracting two fractions together - "self - f2"
+        Parameters:
+        f2 - the other fraction you wish to subtract from "self".
+        Returns:
+        A new fraction where "self" and "f2" have been subtracted together properly.
+        The result is in lowest terms.
+        '''
+
+        ###REMEMBER! To subtract fractions DO NOT subtract the numerators and denominators.
+        ###To add or subtract fractions, they must have a common denominator.
+        ###HINT! You don't need to find the lowest common denominator.
+        ###Use any appropriate common denominator to do the calculation, then reduce it before returning it.
+
+        # todo: do the subtraction!!!
+
+        new_denom = f2.denom * self.denom
+        new_num = f2.denom * self.num - f2.num * self.denom
+
+        frac = Fraction(f"{new_num}/{new_denom}")
+
+        return frac.reduce()
+
+    def __mul__(self, f2):
+        '''
+        The implementation of multiplying two fractions together - "self * f2"
+        Parameters:
+        f2 - the other fraction you wish to multiply by "self".
+
+        Returns:
+        A new fraction where "self" and "f2" have been multiplied together properly.
+        The result is in lowest terms.
+        '''
+
+        ###REMEMBER! To multiply fractions together you multiply the numerators and the denominators.
+        ###Make sure your resulting fraction is in lowest terms before returning it.
+
+        new_num = f2.num * self.num
+        new_denom = f2.denom * self.denom
+
+        frac = Fraction(f"{new_num}/{new_denom}")
+
+        return frac.reduce()
+
+    def __truediv__(self, f2):
+        '''
+        The implementation of multiplying two fractions together - "self / f2"
+        Parameters:
+        f2 - the other fraction you wish to divide by.
+
+        Returns:
+        A new fraction where "self" and "f2" have been divided together properly.
+        The result is in lowest terms.
+        '''
+
+        ###REMEMBER! To divide fractions together you multiply by the reciprocal!
+        ###Make sure your resulting fraction is in lowest terms before returning it.
+
+        new_num = self.num * f2.denom
+        new_denom = self.denom * f2.num
+
+        frac = Fraction(f"{new_num}/{new_denom}")
+
+        return frac.reduce()
+
+    #### Make all of the other comparison operators:
+    #### ==, >, <, >=, <=
+
+    def __eq__(self, f2):
+        f2 = f2.reduce()
+        reduced_self = self.reduce()
+        if f2.num == reduced_self.num and f2.denom == reduced_self.denom:
+            return True
+        else:
+            return False
+
+    def __lt__(self, f2):
+        if self.num * f2.denom < self.denom * f2.num:
+            return True
+        else:
+            return False
+
+    def __le__(self, f2):
+        if self.__lt__(f2) or self.__eq__(f2):
+            return True
+        else:
+            return False
+
+    def __gt__(self, f2):
+        if self.num * f2.denom > self.denom * f2.num:
+            return True
+        else:
+            return False
+
+    def __ge__(self, f2):
+        if self.__gt__(f2) or self.__eq__(f2):
+            return True
+        else:
+            return False
+
+        ###DO NOT CHANGE ANYTHING BELOW HERE###
+
+
+if __name__ == '__main__':
+    frac1 = Fraction("50/100")
+    frac2 = Fraction("2/16")
+
+    print("Printing frac1. Expected output should be\n50/100")
+    print("Actual Result:")
+    print(frac1)
+
+    print("Printing call to float(frac2). Expected output should be\n0.125")
+    print('Actual Result:')
+    print(float(frac2))
+
+    print("Printing call to frac1.get_num(). Expected output should be\n50")
+    print("Actual Result:")
+    print(frac1.get_num())
+
+    print("Printing call to frac2.get_denom(). Expected output should be\n16")
+    print("Actual Result:")
+    print(frac2.get_denom())
+
+    print("Testing call to frac1.reduce(). Expected output should be:\n1/2")
+
+    r = frac1.reduce()
+    print("Actual Result:")
+    print(r)
+
+    print("Testing frac1 + frac2. Expected output should be:\n5/8")
+    fsum = frac1 + frac2
+    print("Actual Result:")
+    print(fsum)
+
+    print("Testing frac2 - frac1. Expected output should be:\n-3/8")
+    fsub = frac2 - frac1
+    print("actual Result:")
+    print(fsub)
+
+    print("New Fractions - fracA is -14/-27 and fracB is 3/7")
+    (fracA, fracB) = (Fraction("-14/-27"), Fraction("3/7"))
+    print("Testing fracA * fracB. Expected output should be:\n2/9")
+    mult = fracA * fracB
+    print("Actual Result:")
+    print(mult)
+
+    print("Testing (-3/7)/(3/-4). Expected output should be:\n4/7")
+    div = Fraction("-3/7") / Fraction("3/-4")
+    print("Actual Result:")
+    print(div)
+    print("######\nCheck the results, fix where necessary\n#####")
+
+    #### Create test cases for the comparison operators.
+
+    print("Testing (-3/7)==(-3/7). Expected output should be:\nTrue")
+    eq = Fraction("-3/7") == Fraction("-3/7")
+    print("Actual Result:")
+    print(eq)
+
+    print("Testing (1/2)<(3/4). Expected output should be:\nTrue")
+    lt = Fraction("1/2") < Fraction("3/4")
+    print("Actual Result:")
+    print(lt)
+
+    print("Testing (1/2)>(3/4). Expected output should be:\nFalse")
+    gt = Fraction("1/2") > Fraction("3/4")
+    print("Actual Result:")
+    print(gt)
+
+    print("Testing (1/2)<=(1/2). Expected output should be:\nTrue")
+    le = Fraction("1/2") <= Fraction("1/2")
+    print("Actual Result:")
+    print(le)
+
+    print("Testing (1/2)>=(3/4). Expected output should be:\nFalse")
+    ge = Fraction("1/2") >= Fraction("3/4")
+    print("Actual Result:")
+    print(ge)
